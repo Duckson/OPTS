bf62122b75f86a310e93feb6d06dd360ed45c3b2
Duckson <vladproff@gmail.com>
2016-05-15 19:58:02 +0300

    Cleanup

--- model/DB.php
+++ model/DB.php
@@ -16,35 +16,6 @@ class DB
         return -1;
     }
 
-    public static function getStudents()
-    {
-        $sql = Config::get()->db;
-        $data = $sql->query("
-            SELECT students.id st_id,
-            	students.first_name st_f_name,
-            	students.last_name st_l_name,
-            	students.patronymic st_patro,
-            	applications.id app_id, 
-            	applications.start_date app_start,
-                applications.end_date app_end,
-            	contracts.id contr_id,
-            	contracts.text contr_text,
-            	contracts.formation_date contr_date,
-            	companies.name company_name
-            FROM students 
-                LEFT JOIN student_app_link ON(student_app_link.student_id = students.id)
-                LEFT JOIN applications ON(student_app_link.app_id = applications.id)
-                LEFT JOIN contracts ON(contracts.id = applications.contract_id)
-                LEFT JOIN companies ON(companies.id = contracts.company_id)
-            ORDER BY students.last_name ASC, students.first_name ASC
-                  ");
-        echo $sql->error;
-        while ($row = $data->fetch_array()) {
-            $all[] = $row;
-        }
-        return $all;
-    }
-
 
     public static function query($query)
     {
@@ -103,12 +74,5 @@ class DB
 
         $sql->query("INSERT INTO contracts(company_id, formation_date, text) VALUES ('$company', '$date', '$text')");
         echo $sql->error;
-    }
-
-    public static function test()
-    {
-        $sql = Config::get()->db;
-        $students_table = $sql->query("SELECT * FROM students");
-        while ($student = $students_table->fetch_array()) $students[] = $student;
     }
 }
